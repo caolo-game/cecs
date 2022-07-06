5374d38032198115a3e448208110337898280d93
Daniel Kiss <littlesnorrboy@gmail.com>
2022-07-06 22:57:33 +0200

    Init set_component

--- src/lib.rs
+++ src/lib.rs
@@ -41,12 +41,15 @@ pub enum WorldError {
 
 pub type WorldResult<T> = Result<T, WorldError>;
 
+pub trait Component: 'static + Clone {}
+impl<T: 'static + Clone> Component for T {}
+
 impl World {
     pub fn new(capacity: u32) -> Self {
         let entity_ids = HandleTable::new(capacity);
 
         let mut archetypes = HashMap::with_capacity(128);
-        let void_store = ArchetypeStorage::new::<()>(128);
+        let void_store = ArchetypeStorage::default();
         archetypes.insert(void_store.ty(), void_store);
 
         // FIXME: can't add assert to const fn...
@@ -64,7 +67,7 @@ impl World {
             .alloc()
             .map_err(|_| WorldError::OutOfCapacity)?;
         let void_store = self.archetypes.get_mut(&VOID_TY).unwrap();
-        void_store.components_mut::<()>().insert(id, ());
+        void_store.set_component(id, ());
         self.entity_archetype.insert(id, VOID_TY);
         Ok(id)
     }
@@ -75,40 +78,64 @@ impl World {
         }
         let ty = self.entity_archetype[&id];
         let archetype = self.archetypes.get_mut(&ty).unwrap();
-        archetype.delete(id);
+        archetype.remove(id);
         self.entity_ids.free(id);
         Ok(())
     }
 
-    pub fn set_component<T>(&mut self, entity_id: EntityId, component: T) -> WorldResult<()> {
+    pub fn set_component<T: Component>(
+        &mut self,
+        entity_id: EntityId,
+        component: T,
+    ) -> WorldResult<()> {
         if !self.entity_ids.is_valid(entity_id) {
             return Err(WorldError::EntityNotFound);
         }
-        let archetype_hash = self.entity_archetype[&entity_id];
-        let archetype = &self.archetypes[&archetype_hash];
+        let mut archetype_hash = self.entity_archetype[&entity_id];
+        let archetype = self.archetypes.get_mut(&archetype_hash).unwrap();
+        if !archetype.contains_column::<T>() {
+            let new_ty = archetype.extended_hash::<T>();
+            if !self.archetypes.contains_key(&new_ty) {
+                self.insert_archetype::<T>(entity_id, archetype_hash)
+            }
+            archetype_hash = new_ty;
+        }
+        let archetype = self.archetypes.get_mut(&archetype_hash).unwrap();
+        archetype.set_component(entity_id, component);
+        Ok(())
+    }
 
-        let old_hash = archetype.ty();
-
-        // let have_already =
-        todo!()
+    #[inline(never)]
+    fn insert_archetype<T: Component>(&mut self, entity_id: EntityId, archetype_hash: TypeHash) {
+        let archetype = self.archetypes.get_mut(&archetype_hash).unwrap();
+        let mut new_arch = archetype.extend_with_column::<T>();
+        // move all components from old archetype to new
+        for (ty, col) in archetype.components.iter_mut() {
+            let dst = new_arch.components.get_mut(ty).unwrap();
+            (col.move_row)(col, dst, entity_id);
+        }
+        let ty = new_arch.ty();
+        self.entity_archetype.insert(entity_id, ty);
+        self.archetypes.insert(ty, new_arch);
     }
 }
 
 #[derive(Clone)]
-pub struct ArchetypeStorage {
+pub(crate) struct ArchetypeStorage {
     ty: TypeHash,
-    components: HashMap<TypeId, ErasedPageTable>,
+    components: HashMap<TypeHash, ErasedPageTable>,
+}
+
+impl Default for ArchetypeStorage {
+    fn default() -> Self {
+        let ty = hash_ty::<()>();
+        let mut components = HashMap::new();
+        components.insert(ty, ErasedPageTable::new(PageTable::<()>::default()));
+        Self { ty, components }
+    }
 }
 
 impl ArchetypeStorage {
-    pub fn new<T: 'static + Clone>(capacity: usize) -> Self {
-        let ty = hash_ty::<T>();
-        Self {
-            ty,
-            components: HashMap::new(),
-        }
-    }
-
     /// Get the archetype storage's ty.
     pub fn ty(&self) -> TypeHash {
         self.ty
@@ -119,15 +146,67 @@ impl ArchetypeStorage {
             storage.remove(id);
         }
     }
+
+    pub fn set_component<T: 'static>(&mut self, id: EntityId, val: T) {
+        unsafe {
+            self.components
+                .get_mut(&hash_ty::<T>())
+                .expect("set_component called on bad archetype")
+                .as_inner_mut()
+                .insert(id, val);
+        }
+    }
+
+    pub fn contains_column<T: 'static>(&self) -> bool {
+        let hash = hash_ty::<T>();
+        self.components.contains_key(&hash)
+    }
+
+    pub fn extended_hash<T: 'static + Clone>(&self) -> TypeHash {
+        if self.contains_column::<T>() {
+            self.ty
+        } else {
+            self.ty ^ hash_ty::<T>()
+        }
+    }
+
+    pub fn extend_with_column<T: 'static + Clone>(&self) -> Self {
+        assert!(!self.contains_column::<T>());
+
+        let mut result = self.clone_empty();
+        let new_ty = self.extended_hash::<T>();
+        result.ty = new_ty;
+        result.components.insert(
+            hash_ty::<T>(),
+            ErasedPageTable::new::<T>(PageTable::default()),
+        );
+        result
+    }
+
+    pub fn clone_empty(&self) -> Self {
+        Self {
+            ty: self.ty,
+            components: HashMap::from_iter(
+                self.components
+                    .iter()
+                    .map(|(id, col)| (*id, (col.clone_empty)())),
+            ),
+        }
+    }
 }
 
 /// Type erased PageTable
-pub struct ErasedPageTable {
+pub(crate) struct ErasedPageTable {
     ty: TypeHash,
     inner: *mut std::ffi::c_void,
     finalize: fn(&mut ErasedPageTable),
     remove: fn(EntityId, &mut ErasedPageTable),
     clone: fn(&ErasedPageTable) -> ErasedPageTable,
+    clone_empty: fn() -> ErasedPageTable,
+    /// src, dst
+    ///
+    /// if component is not in `src` then this is a noop
+    move_row: fn(&mut ErasedPageTable, &mut ErasedPageTable, EntityId),
 }
 
 impl Default for ErasedPageTable {
@@ -138,7 +217,7 @@ impl Default for ErasedPageTable {
 
 impl Drop for ErasedPageTable {
     fn drop(&mut self) {
-        (self.finalize)(&mut self);
+        (self.finalize)(self);
     }
 }
 
@@ -172,6 +251,14 @@ impl ErasedPageTable {
                 let res: PageTable<T> = inner.clone();
                 ErasedPageTable::new(res)
             },
+            clone_empty: || ErasedPageTable::new::<T>(PageTable::default()),
+            move_row: |src, dst, entity_id| unsafe {
+                let src = src.as_inner_mut::<T>();
+                let dst = dst.as_inner_mut::<T>();
+                if let Some(src) = src.remove(entity_id) {
+                    dst.insert(entity_id, src);
+                }
+            },
         }
     }
 
@@ -194,6 +281,6 @@ impl ErasedPageTable {
     }
 
     pub fn remove(&mut self, id: EntityId) {
-        (self.remove)(id, &mut self);
+        (self.remove)(id, self);
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -5,3 +5,14 @@ fn world_deinit_test() {
     let mut w = World::new(500);
     let _id = w.insert_entity().unwrap();
 }
+
+#[test]
+fn can_insert_component_test() {
+    let mut w = World::new(500);
+
+    let id = w.insert_entity().unwrap();
+
+    w.set_component(id, "poggers".to_string()).unwrap();
+    w.set_component(id, 32u32).unwrap();
+    w.set_component(id, 42u64).unwrap();
+}
