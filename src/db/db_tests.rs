220fa1fd6b9b939bc0c6bbf5b169d787ffae59cc
Daniel Kiss <littlesnorrboy@gmail.com>
2022-07-07 17:45:07 +0200

    Init mutable queries

--- src/db/db_tests.rs
+++ src/db/db_tests.rs
@@ -72,3 +72,32 @@ fn non_existent_component_iter_is_empty_test() {
         panic!();
     }
 }
+
+#[test]
+fn mutable_iterator_test() {
+    let mut archetype = ArchetypeStorage::empty()
+        .extend_with_column::<String>()
+        .extend_with_column::<u32>();
+
+    {
+        let id = EntityId::new(0, 1);
+        let index = archetype.insert_entity(id);
+        archetype.set_component(id, index, "pog".to_string());
+        archetype.set_component(id, index, 42u32);
+
+        let id = EntityId::new(32, 1);
+        let index = archetype.insert_entity(id);
+        archetype.set_component(id, index, "pog32".to_string());
+        archetype.set_component(id, index, 69u32);
+    }
+
+    let q = ComponentQuery::<&mut String>::default();
+    for mut s in q.iter_mut(&mut archetype) {
+        *s = "winnie".to_string();
+    }
+
+    let q = ComponentQuery::<&String>::default();
+    for s in q.iter(&archetype) {
+        assert_eq!(&*s, "winnie");
+    }
+}
