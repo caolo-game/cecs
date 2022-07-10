d26337543d50872d04fc4548c1940b49f376e2bc
Daniel Kiss <83577578+danielkissprezi@users.noreply.github.com>
2022-07-10 18:55:20 +0200

    Expose Res and ResMut in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,6 @@
 pub use crate::commands::Commands;
 pub use crate::entity_id::EntityId;
 pub use crate::query::filters::*;
+pub use crate::query::resource_query::*;
 pub use crate::query::Query;
 pub use crate::World;
