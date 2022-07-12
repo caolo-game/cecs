f9164cc476d5d973a7b47fad7e16e1eeb88a64a5
Daniel Kiss <littlesnorrboy@gmail.com>
2022-07-12 14:25:41 +0200

    Add SystemStage to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,4 +3,5 @@ pub use crate::entity_id::EntityId;
 pub use crate::query::filters::*;
 pub use crate::query::resource_query::*;
 pub use crate::query::Query;
+pub use crate::systems::SystemStage;
 pub use crate::World;
