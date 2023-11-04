ef527276504dda1049cdfd43201fa6384bcdef4f
Daniel Kiss <littlesnorrboy@gmail.com>
2023-11-04 21:49:22 +0100

    Rename columns

--- src/archetype.rs
+++ src/archetype.rs
@@ -8,7 +8,7 @@ pub struct ArchetypeStorage {
     pub(crate) ty: TypeHash,
     pub(crate) rows: u32,
     pub(crate) entities: Vec<EntityId>,
-    pub(crate) components: BTreeMap<TypeId, UnsafeCell<ErasedTable>>,
+    pub(crate) components: BTreeMap<TypeId, UnsafeCell<ErasedColumn>>,
 }
 
 unsafe impl Send for ArchetypeStorage {}
@@ -60,7 +60,7 @@ impl ArchetypeStorage {
         let mut components = BTreeMap::new();
         components.insert(
             TypeId::of::<()>(),
-            UnsafeCell::new(ErasedTable::new::<()>(0)),
+            UnsafeCell::new(ErasedColumn::new::<()>(0)),
         );
         Self {
             ty,
@@ -209,7 +209,7 @@ impl ArchetypeStorage {
             let new_ty = self.extended_hash::<T>();
             self.ty = new_ty;
             self.components
-                .insert(TypeId::of::<T>(), UnsafeCell::new(ErasedTable::new::<T>(0)));
+                .insert(TypeId::of::<T>(), UnsafeCell::new(ErasedColumn::new::<T>(0)));
         }
         self
     }
@@ -263,16 +263,16 @@ impl ArchetypeStorage {
     pub fn get_component<T: 'static>(&self, row: RowIndex) -> Option<&T> {
         self.components
             .get(&TypeId::of::<T>())
-            .and_then(|columns| unsafe { (*columns.get()).as_slice().get(row as usize) })
+            .and_then(|rows| unsafe { (*rows.get()).as_slice().get(row as usize) })
     }
 
     pub fn get_component_mut<T: 'static>(&self, row: RowIndex) -> Option<&mut T> {
         self.components
             .get(&TypeId::of::<T>())
-            .and_then(|columns| unsafe { (*columns.get()).as_slice_mut().get_mut(row as usize) })
+            .and_then(|rows| unsafe { (*rows.get()).as_slice_mut().get_mut(row as usize) })
     }
 
-    pub fn components(&self) -> impl Iterator<Item = (TypeId, &ErasedTable)> {
+    pub fn components(&self) -> impl Iterator<Item = (TypeId, &ErasedColumn)> {
         self.components
             .iter()
             .map(|(ty, e)| (*ty, unsafe { &*e.get() }))
@@ -280,7 +280,7 @@ impl ArchetypeStorage {
 }
 
 /// More or less a type erased Vec that holds Components
-pub struct ErasedTable {
+pub struct ErasedColumn {
     pub ty_name: &'static str,
     // Vec //
     data: *mut u8,
@@ -288,46 +288,46 @@ pub struct ErasedTable {
     capacity: usize,
     layout: Layout,
     // Type Erased Methods //
-    pub(crate) finalize: fn(&mut ErasedTable),
+    pub(crate) finalize: fn(&mut ErasedColumn),
     /// remove is always swap_remove
-    pub(crate) remove: fn(RowIndex, &mut ErasedTable),
+    pub(crate) remove: fn(RowIndex, &mut ErasedColumn),
     #[cfg(feature = "clone")]
-    pub(crate) clone: fn(&ErasedTable) -> ErasedTable,
-    pub(crate) clone_empty: fn() -> ErasedTable,
+    pub(crate) clone: fn(&ErasedColumn) -> ErasedColumn,
+    pub(crate) clone_empty: fn() -> ErasedColumn,
     /// src, dst
     ///
     /// if component is not in `src` then this is a noop
     /// Caller must ensure that both tables have the same underlying type
-    pub(crate) move_row: fn(&mut ErasedTable, &mut ErasedTable, RowIndex),
+    pub(crate) move_row: fn(&mut ErasedColumn, &mut ErasedColumn, RowIndex),
     /// src, dst
     /// Move the row from src to the specified slow in dst
     ///
     /// Caller must ensure that dst is initialized and both tables have the same underlying type
-    pub(crate) move_row_into: fn(&mut ErasedTable, RowIndex, &mut ErasedTable, RowIndex),
+    pub(crate) move_row_into: fn(&mut ErasedColumn, RowIndex, &mut ErasedColumn, RowIndex),
     /// Swap rows in an entity
-    pub(crate) swap_rows: fn(&mut ErasedTable, RowIndex, RowIndex),
+    pub(crate) swap_rows: fn(&mut ErasedColumn, RowIndex, RowIndex),
 }
 
-impl Default for ErasedTable {
+impl Default for ErasedColumn {
     fn default() -> Self {
         Self::new::<()>(0)
     }
 }
 
-impl Drop for ErasedTable {
+impl Drop for ErasedColumn {
     fn drop(&mut self) {
         (self.finalize)(self);
     }
 }
 
 #[cfg(feature = "clone")]
-impl Clone for ErasedTable {
+impl Clone for ErasedColumn {
     fn clone(&self) -> Self {
         (self.clone)(self)
     }
 }
 
-impl std::fmt::Debug for ErasedTable {
+impl std::fmt::Debug for ErasedColumn {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("ErasedVec")
             .field("ty", &self.ty_name)
@@ -335,7 +335,7 @@ impl std::fmt::Debug for ErasedTable {
     }
 }
 
-impl ErasedTable {
+impl ErasedColumn {
     pub fn new<T: crate::Component>(capacity: usize) -> Self {
         let layout = Self::layout::<T>(capacity);
         Self {
@@ -344,7 +344,7 @@ impl ErasedTable {
             end: 0,
             data: unsafe { std::alloc::alloc(layout) },
             layout,
-            finalize: |erased_table: &mut ErasedTable| {
+            finalize: |erased_table: &mut ErasedColumn| {
                 // drop the inner table
                 unsafe {
                     let data: *mut T = erased_table.data.cast();
@@ -354,12 +354,12 @@ impl ErasedTable {
                     std::alloc::dealloc(erased_table.data, erased_table.layout);
                 }
             },
-            remove: |entity_id, erased_table: &mut ErasedTable| unsafe {
+            remove: |entity_id, erased_table: &mut ErasedColumn| unsafe {
                 erased_table.swap_remove::<T>(entity_id as usize);
             },
             #[cfg(feature = "clone")]
-            clone: |table: &ErasedTable| {
-                let mut res = ErasedTable::new::<T>(table.capacity);
+            clone: |table: &ErasedColumn| {
+                let mut res = ErasedColumn::new::<T>(table.capacity);
                 res.end = table.end;
                 for i in 0..table.end {
                     unsafe {
@@ -369,7 +369,7 @@ impl ErasedTable {
                 }
                 res
             },
-            clone_empty: || ErasedTable::new::<T>(1),
+            clone_empty: || ErasedColumn::new::<T>(1),
             move_row: |src, dst, index| unsafe {
                 let src = src.swap_remove::<T>(index as usize);
                 dst.push::<T>(src);
