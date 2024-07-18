9a56c811f7d0e29874fcef7148ecf32b1d15d339
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-19 01:07:47 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "cecs devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    templ.url = "github:a-h/templ";
+  };
+
+  outputs = inputs@{ self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) inputs.templ.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        templ = inputs.templ.packages.${system}.templ;
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            (rust-bin.nightly.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" "rustfmt" ];
+              targets = [ ];
+            })
+            cargo-nextest
+            cargo-edit
+            just
+            stdenv.cc.cc
+          ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath [
+          ];
+        };
+      }
+    );
+}
+
