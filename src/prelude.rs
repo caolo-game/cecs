87dfb5b7055ce4f9a4633f30576dfb17d4b6f050
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-20 10:08:40 +0200

    chore: Fix unused warning for QuerySets

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,7 +2,7 @@ pub use crate::bundle::Bundle;
 pub use crate::commands::Commands;
 pub use crate::entity_id::EntityId;
 pub use crate::query::{filters::*, resource_query::*, Has, Query};
-pub use crate::query_set::*;
+pub use crate::query_set::QuerySet;
 pub use crate::systems::{IntoSystem, SystemStage};
 pub use crate::table::ArchetypeHash;
 pub use crate::world_access::WorldAccess;
