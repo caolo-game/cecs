a785fefa960a3afce46571d44b05198b6a8fee78
Daniel Kiss <littlesnorrboy@gmail.com>
2024-08-07 01:55:49 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -5,33 +5,30 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    templ.url = "github:a-h/templ";
   };
 
   outputs = inputs@{ self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) inputs.templ.overlays.default ];
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        templ = inputs.templ.packages.${system}.templ;
       in
       with pkgs;
       {
         devShells.default = mkShell {
           buildInputs = [
             (rust-bin.nightly.latest.default.override {
-              extensions = [ "rust-src" "rust-analyzer" "rustfmt" ];
+              extensions = [ "rust-src" "rust-analyzer" "rustfmt" "clippy" ];
               targets = [ ];
             })
             cargo-nextest
             cargo-edit
             cargo-all-features
             just
-            stdenv.cc.cc
-          ];
-          LD_LIBRARY_PATH = lib.makeLibraryPath [
+            git
+            git-cliff
           ];
         };
       }
