04ff75ff4cf9c66fe654c0846fbcf61fd2fcd7a3
Daniel Kiss <littlesnorrboy@gmail.com>
2025-06-07 00:07:57 +0200

    chore: Update deps

--- src/lib.rs
+++ src/lib.rs
@@ -663,7 +663,7 @@ impl World {
     }
 
     /// Constructs a new [crate::commands::Commands] instance with initialized buffers in this world
-    pub fn ensure_commands(&mut self) -> prelude::Commands {
+    pub fn ensure_commands(&mut self) -> prelude::Commands<'_> {
         self.resize_commands(1);
         commands::Commands::new(self, 0)
     }
